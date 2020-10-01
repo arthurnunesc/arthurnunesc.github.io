8f0ef4b031638dd4e8eb512a19ff3f264d3cb8d2
Arthur Nunes <arthurnunes@tutanota.com>
2020-09-30 21:48:05 -0300

    Make burger menu slide back when a link is clicked on mobile

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let i = 0;
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
@@ -14,9 +15,29 @@ const navSlide = () => {
           index / 7 + 0.3
         }s`;
       }
+      i++;
     });
     // Burger Animation
     burger.classList.toggle("toggle");
+  });
+  nav.addEventListener("click", () => {
+    if (i % 2 != 0) {
+      // Toggle Nav
+      nav.classList.toggle("nav-active");
+      // Animate Links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`;
+        }
+        i++;
+      });
+      // Burger Animation
+      burger.classList.toggle("toggle");
+    }
   });
 };
 
